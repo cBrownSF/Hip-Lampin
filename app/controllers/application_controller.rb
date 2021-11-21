aa87e31a9180fbcc15b216c9fc44f72fbe85b26c
Cal Brown <calsbrown4@gmail.com>
2021-11-21 01:15:43 -0800

    app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   skip_forgery_protection
   helper_method :current_user, :logged_in?
-
-  private
 
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    unless logged_in?
+      render json: { base: ['invalid credentials'] }, status: 401 
+    end
   end
 
   def logged_in?
@@ -16,20 +20,13 @@ class ApplicationController < ActionController::Base
 
   def login!(user)
     user.reset_session_token!
-    session[:session_token] = user.session_token
-    @current_user = user
+    session[:session_token] = user.reset_session_token!
+
   end
 
-  def logout
+  def logout!
     current_user.reset_session_token!
     session[:session_token] = nil
-    @current_user = nil
-  end
-
-  def require_logged_in
-    unless current_user
-      render json: { base: ['invalid credentials'] }, status: 401
-    end
   end
 
 end
--- frontend/hipentry.jsx
+++ frontend/hipentry.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from './root.jsx'
-import {login,logout,signup} from './actions/session_actions'
+import { login, logout } from './actions/session_actions'
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore()
   console.log(store)
-  ReactDOM.render(<Root store={store}/>,root);
+  ReactDOM.render(<Root store={store} />, root);
   window.store = store
   window.login = login
-  window.logout =logout
-  window.signup = signup
+  window.logout = logout
 });
-
