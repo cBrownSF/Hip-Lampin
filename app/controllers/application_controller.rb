1410c298e35469e78fe9725f98c0e0ca71112ecf
Cal Brown <calsbrown4@gmail.com>
2021-11-30 01:27:32 -0800

    Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,13 @@ class ApplicationController < ActionController::Base
   def login!(user)
     user.reset_session_token!
     session[:session_token] = user.reset_session_token!
-
+   
   end
 
   def logout!
     current_user.reset_session_token!
     session[:session_token] = nil
+    
   end
 
 end
