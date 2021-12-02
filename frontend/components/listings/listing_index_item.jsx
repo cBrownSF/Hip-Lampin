c402431b1bdf71a07ab57734890ad4e89f533f4b
Cal Brown <calsbrown4@gmail.com>
2021-12-02 15:54:32 -0800

    photo

--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const ListingIndexItem = (props) => {
-  debugger;
   return <li>
     <Link to={`/listings/${props.listings.id}`}>{props.listings.name}</Link>
-    
+    <img src={props.listings.photoURL} />
   </li>
 }
 
