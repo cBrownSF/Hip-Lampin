dceaa6bd95201ee7fd3634fccc61462a33989c01
Cal Brown <calsbrown4@gmail.com>
2021-12-03 07:40:19 -0800

    Issue

--- frontend/components/listings/listing_show.jsx
+++ frontend/components/listings/listing_show.jsx
@@ -6,8 +6,8 @@ class ListingShow extends React.Component {
   componentDidMount() {
     this.props.receiveListing(this.props.match.params.listingId);
   }
+  
   componentDidUpdate() {
-    console.log(this.props)
     if (!this.props.listing){
       this.props.receiveListing(this.props.match.params.listingId);
     }
@@ -25,15 +25,11 @@ class ListingShow extends React.Component {
   updatedProps(word){
     return word === true ? 'available': 'unavailable'
   }
-  
-  showEdit(){
-    if (this.props.currentUser.id === this.props.listing.host_id){
-      return <p id="link-location"><Link id='show-link' to={descript}>Edit</Link></p>
-    }else{
-      return <p></p>
-    }
-  }
 
+  updatedYesProps(word) {
+    return word === true ? 'Yes' : 'No'
+  }
+  
 render() {
  
   if (!this.props.listing){
@@ -41,6 +37,7 @@ render() {
   }
 
   const listing = this.props.listing
+  
   const descript = {
     pathname: `/listings/${this.props.listing.id}/edit`,
     search:'2',
@@ -61,6 +58,13 @@ render() {
     search: '1',
     state: this.props.listing
   }
+  const onEdit= ()=>{
+    if (this.props.currentUser.id === this.props.listing.host_id) {
+    return <p id="link-location"><Link id='show-link' to={descript}>Edit</Link></p>
+  } else {
+    return <p></p>
+  }
+}
   return(
    
     <div className='show-container'>
@@ -69,11 +73,9 @@ render() {
         <img src={listing.photoURL} width="925" height = '300' alt="coverphoto" />
       </div>
       <div id="side-pic"></div>
-
-      
       <div id="left"></div>
       <div id = 'name-show'>
-        <p id="link-location"><Link id = 'show-link'to={nameedit}>Edit</Link></p>
+        <p id="link-location">{onEdit()}</p>
         <h1>{listing.name}</h1>
           <p id = 'nearby-show'>Nearby: Golden Gate Bridge</p>
           <hr id="solid" />
@@ -83,8 +85,6 @@ render() {
         <span id="price">{`$${listing.cost}`}</span>
         <p id='per-night'>per night(2 guests)</p>
         
-      
-       
       </div>
       <div id="left"></div>
       <div id= 'descript-show'>
@@ -98,10 +98,11 @@ render() {
         <p id="link-location"><Link id='show-link' to={amenities}>Edit</Link></p>
           <p id='headers'>Amenities</p>
         <ul>
-          <li id="text">{`Trash cans ${this.updatedProps(listing.is_trash)}`}</li>
+          <li id="text">{`Trash cans are ${this.updatedProps(listing.is_trash)}`}</li>
           <li id="text">{`Showers ${this.updatedProps(listing.is_shower)}`}</li>
           <li id="text">{`Wifi ${this.updatedProps(listing.is_wifi)}`}</li>
           <li id="text">{`Picnic tables ${this.updatedProps(listing.is_picnic_table)}`}</li>
+          <li id="text">{`Kitchen ${this.updatedProps(listing.is_kitchen)}`}</li>
         </ul>
         </div>
 
@@ -114,30 +115,33 @@ render() {
           </ul>
         </div>
 
-        <div id='activities-show'>
-          <span id='headers'>Activities</span>
-          <span>{listing.is_hiking}</span>
-          <span>{listing.is_swimming}</span>
-          <span>{listing.is_paddling}</span>
-          <span>{listing.is_wildlife}</span>
-          <span>{listing.is_fishing}</span>
-        </div>
-  <div id="right"></div>
+      <div id='activities-show'>
+        <p id='headers'>Activities</p>
+        <ul>
+          <li id='text'>{`Hiking: ${this.updatedYesProps(listing.is_hiking)}`}</li>
+          <li id='text'>{`Swimming: ${this.updatedYesProps(listing.is_swimming)}`}</li>
+          <li id='text'>{`Paddling: ${this.updatedYesProps(listing.is_paddling)}`}</li>
+          <li id='text'>{`Wildlife:${this.updatedYesProps(listing.is_wildlife)}`}</li>
+          <li id='text'>{`Fishing: ${this.updatedYesProps(listing.is_fishing)}`}</li>
+      </ul>
+      </div>
+      <div id="right"></div>
 
       <div id="left"></div>
       <div id="details-show">
         <span>Details</span>
         <ul>
-          <li className="list-item">On arrival: <span className="list-item-data">{listing.on_arrival}</span></li>
-          <li className="list-item">Minimum nights: <span className="list-item-data">{listing.on_arrival}</span></li>
-          <li className="list-item">Check in: <span className="list-item-data">{`After ${listing.check_in_time}`}</span></li>
-          <li className="list-item">Check out: <span className="list-item-data">{`${listing.check_in_time} night`}</span></li>
+          <li id="list-item">On arrival: <span className="list-item-data">{listing.on_arrival}</span></li>
+          <li id="list-item">Minimum nights: <span className="list-item-data">{listing.minimum_night}</span></li>
+          <li id="list-item">Check in: <span className="list-item-data">{`After ${listing.check_in_time}`}</span></li>
+          <li id="list-item">Check out: <span className="list-item-data">{`${listing.check_in_time} night`}</span></li>
           <li className="list-item">Cancellation policy: <span className="list-item-data">{listing.cancellation_policy}</span></li>
           <li className="list-item">Check out: <span className="list-item-data">{`Before ${listing.check_in_time}`}</span></li>
           <li className="list-item">Response time: <span className="list-item-data">{`Within ${listing.response_time}`}</span></li>
           <li className="list-item">Accepts Booking: <span className="list-item-data">{listing.booking_time}</span></li>
         </ul>
       </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aspernatur inventore, voluptatem vitae illum odio expedita aut, harum laboriosam natus accusamus mollitia praesentium exercitationem. Deserunt maxime, illo esse velit optio ut quibusdam dolores non vero saepe ipsa quidem mollitia placeat, temporibus veniam tempora porro voluptatum accusamus incidunt voluptas aut natus. Quia suscipit voluptatibus officia quasi quidem impedit sed obcaecati commodi.</p>
       <div id="right"></div>
       <div>
         <button id='show-delete-button' onClick={()=>this.onDelete()}>delete listing</button>
