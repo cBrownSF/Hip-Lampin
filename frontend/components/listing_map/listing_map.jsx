e8b7f8c77e93770134400d583733696572136a87
Cal Brown <calsbrown4@gmail.com>
2021-12-20 19:15:32 -0800

    Search Container Continued

--- frontend/components/listing_map/listing_map.jsx
+++ frontend/components/listing_map/listing_map.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 class ListingMap extends React.Component {
-
+componentDidMount(){
+  const mapOptions = {
+    center: { lat: 37.7758, lng: -122.435 }, 
+    zoom: 13
+  };
+  this.map = new google.maps.Map(this.mapNode, mapOptions);
+}
 render(){
   return(
-  <div className='map' ref='map'>
+    <div className='map' ref={ map => this.mapNode = map }>
     Map
   </div>
   )
--- frontend/components/search/listings_index.jsx
+++ frontend/components/search/listings_index.jsx
@@ -1,34 +1,37 @@
 import React from "react";
 import ListingIndexItem from "./listing_index_item";
 
-class ListingIndex extends React.Component{
-  constructor(props){
-    super(props)
-  }
-  componentDidMount(){
-    // debugger;
-    // // this.props.receiveListings()
-  }
+const ListingIndex = (props) => {
   
-  render(){
-    if (Object.keys(props.listing).length === 0) {
-      debugger;
+    if (Object.keys(props.listings).length === 0) {
       return null;
-    }
-    const Array = Object.values(this.props.listings)
-    debugger;
-    return (
-      <div>
-        <h1>Listing: </h1>
-          {Array.map(listing =>(
+    }else{
+      return(
+        <div>
+          <h1>Listing: </h1>
+          {Object.values(props.listings).map(listing => (
             <ListingIndexItem
-            listings={listing}
-            key={listing.id}
+              listings={listing}
+              key={listing.id}
             />
           ))}
-      </div>
-    )
+        </div>
+      )
+    }
   }
-}
+  //   const Array = Object.values(this.props.listings)
+  //   return (
+  //     <div>
+  //       <h1>Listing: </h1>
+  //         {Array.map(listing =>(
+  //           <ListingIndexItem
+  //           listings={listing}
+  //           key={listing.id}
+  //           />
+  //         ))}
+  //     </div>
+  //   )
+  // }
+
 
 export default ListingIndex;
