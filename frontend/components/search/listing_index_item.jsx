bddbd6a095565a0644459238acc679070c494df9
Cal Brown <calsbrown4@gmail.com>
2022-01-21 10:18:18 -0800

    New API ENV KEY SET

--- frontend/components/search/listing_index_item.jsx
+++ frontend/components/search/listing_index_item.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const ListingIndexItem = (props) => {
+
   return <li>
     <img src={props.listings.photoURL} height="200px" width="200px"/>
     <p className='index-item'><Link className ="index-link" to={`/listings/${props.listings.id}`}>{props.listings.name}</Link></p>
