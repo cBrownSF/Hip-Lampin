b3a5647b5f70648d5627c0f9a5e8b07a3a0c8fe6
Cal Brown <calsbrown4@gmail.com>
2022-02-03 17:48:59 -0800

    Review is Created

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import listingsReducer from "./listings_reducer";
+import reviewsReducer from "./reviews_reducer";
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  listings: listingsReducer
+  listings: listingsReducer,
+  reviews: reviewsReducer
 });
 
 export default entitiesReducer;
--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -3,14 +3,18 @@ import { RECEIVE_REVIEW,RECEIVE_LISTING } from "../actions/listings_actions";
 
 const reviewsReducer = (oldState={},action) => {
   Object.freeze(oldState)
+  debugger;
     switch (action.type) {
+    
       case RECEIVE_REVIEW:
         debugger;
+        console.log('hit recieve review')
         return  Object.assign({}, oldState, { [action.review.id]: action.review })
       case RECEIVE_LISTING:
         debugger;
         return   Object.assign({}, oldState, action.reviews)
       default:
+        'hit default'
         return oldState;
     }
 }
