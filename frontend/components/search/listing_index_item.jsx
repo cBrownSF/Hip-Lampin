d5c8226fd93339aa4fafc44550899eae47fbba10
Cal Brown <calsbrown4@gmail.com>
2022-02-11 16:34:05 -0800

    Index page styled

--- frontend/components/search/listing_index_item.jsx
+++ frontend/components/search/listing_index_item.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const ListingIndexItem = (props) => {
-
-  return <li>
-    <img src={props.listings.photos[0]} height="200px" width="200px"/>
-    <p className='index-item'><Link className ="index-link" to={`/listings/${props.listings.id}`}>{props.listings.name}</Link></p>
+console.log(props)
+  return <li className='index-item-li'>
+    <div className="listing-index-item-box">
+      <img className="index-photo"src={props.listings.photos[0]} height="200px" width="200px"/>
+      <Link className='link-over-text-box' to={`/listings/${props.listings.id}`}> <div className='text-index-box'>
+      {/* <Link className ="index-link" to={`/listings/${props.listings.id}`}>{props.listings.name}</Link> */}
+        <p className="index-link" >{props.listings.name}</p>
+        <p className="index-location-cost">{props.listings.city},{props.listings.state}</p>
+        <p className="index-location-cost">From ${props.listings.cost}/per night</p>
+  </div></Link>
+    </div>
   </li>
 }
 
--- frontend/components/search/listings_index.jsx
+++ frontend/components/search/listings_index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ListingIndexItem from "./listing_index_item";
-
+import { Link } from "react-router-dom";
 const ListingIndex = (props) => {
   
     if (Object.keys(props.listings).length === 0) {
       return null;
     }else{
+      console.log(props.listings.id)
+      debugger;
       return(
         <div className="index-item">
+
           {Object.values(props.listings).map(listing => (
             <ListingIndexItem
               listings={listing}
