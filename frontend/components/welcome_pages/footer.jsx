69d4557c2237b12d162eaeb698babf8b8defca43
Cal Brown <calsbrown4@gmail.com>
2022-07-12 15:30:12 -0700

    github and CSS added

--- frontend/components/welcome_pages/footer.jsx
+++ frontend/components/welcome_pages/footer.jsx
@@ -10,10 +10,12 @@ export default function Footer() {
               {/* <img class="linkedin" src="./assets/Linkedin-logo-on-transparent-Background-PNG-.png" width="60" height="60" alt="LinkedIn Logo" /> */}
             </a></li>
             <li><a href='https://github.com/cBrownSF' target='_blank'>
-              <p>Github</p>
-              <img class="github" src="./assets/github-icon-white-github-icon-black-background-symbol-logo-trademark-steering-wheel-transparent-png-842663.png" width="60" height="60" alt="github Logo" />
+                {/* <FontAwesomeIcon icon="fab fa-github" /> */}
+            <i className="fab fa-github" style={{ fontSize: '20' }}></i>
+              {/* <img class="github" src="./assets/github-icon-white-github-icon-black-background-symbol-logo-trademark-steering-wheel-transparent-png-842663.png" width="60" height="60" alt="github Logo" /> */}
           </a></li>
-          <li>Repository</li>
+          <li><a href= "https://github.com/cBrownSF/Hip-Lampin">Repository</a></li>
+          <li><a href = "https://cbrownsf.github.io/portfolio/">Portfolio</a></li>
         </ul>
       </nav>
 
