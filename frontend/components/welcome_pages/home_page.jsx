d309f31eb493983fd80a917335c8a11ecc92b044
Cal Brown <calsbrown4@gmail.com>
2022-07-22 14:33:22 -0700

    right-photo is set

--- frontend/components/welcome_pages/home_page.jsx
+++ frontend/components/welcome_pages/home_page.jsx
@@ -40,13 +40,16 @@ class HomePage extends React.Component {
       <div className='home-page-link-div' >
           <div className="home-page-link-photo-left">
             <div className ='div-image'>
-              <img className= "img-home-page" src = "https://hip-lamping-cover-photos.s3.us-west-1.amazonaws.com/rhett-house+copy.jpg"></img>
+              <img className= "img-home-page" src = "https://hip-lamping-cover-photos.s3.us-west-1.amazonaws.com/rhett-house+copy.jpg" alt= 'Beaufort'></img>
             </div>
             <div className='card-cover'>
               <Link className="link-yos-carolina" to={southCarolinaProps}><p className="font-main-page-photo">Carolina Hideaways</p></Link>
             </div>
           </div>
           <div className="home-page-link-photos">
+            <div className ='div-image'>
+              <img className = 'img-home-page' src="https://hip-lamping-cover-photos.s3.us-west-1.amazonaws.com/IMG_2155.jpg" alt="Montara" />
+            </div>
             <div className='card-cover-right'>
               <Link className="link-yos-carolina"to={yosemiteProps}><p className="font-main-page-photo">Montara Getaways</p></Link>
             </div>
