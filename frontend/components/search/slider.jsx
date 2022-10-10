7241515473aca5380f8b6fa8889757695c7b0211
Cal Brown <calsbrown4@gmail.com>
2022-10-09 22:01:54 -0700

    carousel buttons made bigger and hover functionality lessened

--- frontend/components/search/slider.jsx
+++ frontend/components/search/slider.jsx
@@ -26,14 +26,14 @@ export default function Slider({ photos, page }) {
         <div className={`buttons-class-${page}`}>
           {slide !== 1 ? (
           <button className="carousel-button" onClick={() => prevImage()}>
-            <i class="fas fa-chevron-left" style={{ color: 'white' }}></i>
+            <i class="fas fa-chevron-left fa-2xl" style={{ color: 'white' }}></i>
           </button> ) : 
           (<button className="carousel-button-hidden" disabled>
             <i class="fas fa-chevron-right" style={{ color: 'white' }}></i>
           </button> )}
           {slide !== 3 ? (
           <button className="carousel-button" onClick={() => nextImage()}>
-            <i class="fas fa-chevron-right" style={{ color: 'white' }}></i>
+            <i class="fas fa-chevron-right fa-2xl" style={{ color: 'white' }}></i>
           </button>) :
           (<button className="carousel-button-hidden" disabled>
             <i class="fas fa-chevron-right" style={{ color: 'white' }}></i>
