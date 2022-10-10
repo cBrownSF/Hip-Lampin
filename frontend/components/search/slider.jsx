987963e1ae7c01fa0f41b7b88f7b16dc8a033939
Cal Brown <calsbrown4@gmail.com>
2022-10-09 19:48:08 -0700

    slider component created

---
+++ frontend/components/search/slider.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+export default function Slider({photos}) {
+ 
+
+  const [slide,setSlide] = useState(1);
+
+  const prevImage = () => {
+    if (slide !== 1) {
+      setSlide(prevCount => prevCount - 1);
+    } else {
+      setSlide(3);
+    }
+  }
+  const nextImage = () => {
+    if (slide !==3) {
+      setSlide(prevCount => prevCount + 1) 
+    } else {
+      setSlide(1);
+    }
+  }
+
+  return (
+    <div className="slider-container">
+      <div className='slider-div'>
+        <img className="index-photo" src={photos[slide - 1]} height="200px" width="200px" />
+        <div className="buttons-class">
+          <button className="carousel-button" onClick={() => prevImage()}><i class="fas fa-chevron-left" style={{ color: 'white' }}></i></button>
+          <button className="carousel-button" onClick={() => nextImage()}><i class="fas fa-chevron-right" style={{ color: 'white' }}></i></button>
+        </div>
+      </div>
+    </div>
+  )
+}
